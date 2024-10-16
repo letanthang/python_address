12b180a065012a7307750f7b3eb9e07ea35e26e7
Phuoc <ngmphuoc.work@gmail.com>
2024-10-16 20:44:27 +0700

    update nomalization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import logging
 from trie import Trie
 from trie import dynamic_parse
@@ -36,7 +37,54 @@ def classify_address(input_string, trie_dic):
     return result
 
 def normalize_input(input_string):
-    return input_string.lower()
+        
+
+        def no_accent_vietnamese(input_string):
+            input_string = re.sub('[áãăắằẳẵặấầẫậàạâ]', 'a', input_string) # ạâảẩ
+            # input_string = re.sub('[ÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ]', 'A', input_string)
+            # input_string = re.sub(u'Đ', 'D', input_string)
+            input_string = re.sub(u'đ', 'd', input_string)
+            input_string = re.sub('[éèẻẽẹêếềểễệ]', 'e', input_string)
+            # input_string = re.sub('[ÉÈẺẼẸÊẾỀỂỄỆ]', 'E', input_string)
+            input_string = re.sub('[õọóòôốồổỗộơớờởỡợ]', 'o', input_string) # ỏ
+            # input_string = re.sub('[ỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ]', 'O', input_string) # ÓÒ
+            input_string = re.sub('[íìỉĩị]', 'i', input_string)
+            # input_string = re.sub('[ÍÌỈĨỊ]', 'I', input_string)
+            input_string = re.sub('[úủũưứừửữự]', 'u', input_string) # ù
+            # input_string = re.sub('[ÚÙỦŨỤƯỨỪỬỮỰ]', 'U', input_string)
+            input_string = re.sub('[ýỳỷỹỵ]', 'y', input_string)
+            # input_string = re.sub('[ÝỲỶỸỴ]', 'Y', input_string)
+            return input_string
+
+        # Remove "." at the end
+        processed_string = input_string[:-1] if input_string[-1] == "." else input_string
+        
+        # Lower string first
+        processed_string = processed_string.lower()
+
+        # Define a regex pattern to match numbers not preceded by the word "phường", "quận"
+        pattern = r"(?<!quận )(?<!q)(?<!q\.)(?<!quận \d)(?<!q\d)(?<!q\.\d)(?<!phường )(?<!p)(?<!p\.)(?<!phường \d)(?<!p\d)(?<!p\.\d)\d+"
+
+        # Replace matched numbers with an empty string
+        processed_string = re.sub(pattern, "", processed_string)
+
+        # Make list of delimiters         
+        delimiters = [
+                "t.x.", "t.", "t.t.", "h.",
+                "thành phố", "thành fhố", "tỉnh", "tp.", "t.phố"
+                "quận", "huyện", "huyên", "thị xã", "q.",  "tx",
+                "phường", "xã", "thị trấn", "f.", "x.",
+                "khu phố", "khóm", "thôn", "ấp",
+                " ", "-", ".", ",", "/", "số"
+                ]
+
+        for d in delimiters:
+            processed_string = processed_string.replace(d, "")
+        processed_string = no_accent_vietnamese(processed_string)
+
+        # return processed_string
+        return processed_string
+
 
 def import_dictionary(file_name):
     trie_dic = Trie()
