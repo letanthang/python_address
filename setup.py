7614564ac6e05390ba53578c6531ae01398fce9e
Minton <letanthang@gmail.com>
2024-11-13 10:41:29 +0700

    dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_address',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python library for address parsing and validation.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,7 +10,7 @@ setup(
     author_email='letanthang@gmail.com',
     url='https://github.com/letanthang/python_address',
     package_data={
-        'python_address': ['assets/*'],  # Specifies the files to include
+        'python_address': ['assets/wards.csv'],  # Specifies the files to include
     },
     packages=find_packages(),
     install_requires=[],
